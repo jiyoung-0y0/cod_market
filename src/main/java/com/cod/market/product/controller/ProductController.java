7f2c92422772b46359b5653e71b51486aa75d96c
jiyoung-0y0 <kimwldud1234@gmail.com>
2024-07-08 17:48:41 +0900

    product detail view

--- src/main/java/com/cod/market/product/controller/ProductController.java
+++ src/main/java/com/cod/market/product/controller/ProductController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -21,5 +22,12 @@ public class ProductController {
         List<Product> productsList = productService.getList();
         model.addAttribute("productList", productsList);
         return "product/list";
+    }
+
+    @GetMapping("/detail/{id}")
+    public String detail(@PathVariable("id") Long id, Model model){
+
+
+        return "product/detail";
     }
 }
