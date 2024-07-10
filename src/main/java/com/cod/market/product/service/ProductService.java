4d8616f5ec9bb6da2a4cbccf73d65d505f6249be
jiyoung-0y0 <kimwldud1234@gmail.com>
2024-07-10 14:20:51 +0900

    detail data

--- src/main/java/com/cod/market/product/service/ProductService.java
+++ src/main/java/com/cod/market/product/service/ProductService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -25,5 +26,12 @@ public class ProductService {
         p.setPrice(price);
         p.setCreateDate(LocalDateTime.now());
         productRepository.save(p);
+    }
+
+    public Product getProduct(Long id) {
+        Optional<Product> product = productRepository.findById(id);
+
+        // TODO: 없을 경우 예외처리 예정
+        return product.get();
     }
 }
