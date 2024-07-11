a7b780d802f883d05bb73246ba293b20888bad6b
jiyoung-0y0 <kimwldud1234@gmail.com>
2024-07-11 16:30:16 +0900

    list paging

--- src/main/java/com/cod/market/product/controller/ProductController.java
+++ src/main/java/com/cod/market/product/controller/ProductController.java
@@ -3,13 +3,13 @@ package com.cod.market.product.controller;
 import com.cod.market.product.entity.Product;
 import com.cod.market.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,10 +17,10 @@ import java.util.List;
 public class ProductController {
 
     private final ProductService productService;
-    @GetMapping("list")
-    public String list(Model model){
-        List<Product> productsList = productService.getList();
-        model.addAttribute("productList", productsList);
+    @GetMapping("/list")
+    public String list(Model model, @RequestParam(value="page", defaultValue = "0")int page){
+        Page<Product> paging = productService.getList(page);
+        model.addAttribute("paging", paging);
         return "product/list";
     }
 
