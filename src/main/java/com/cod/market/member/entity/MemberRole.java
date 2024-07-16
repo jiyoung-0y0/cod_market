86b5c7828dc4b65684a149b92f2591c7af6becd5
jiyoung-0y0 <kimwldud1234@gmail.com>
2024-07-16 15:27:11 +0900

    add MemberRole

---
+++ src/main/java/com/cod/market/member/entity/MemberRole.java
@@ -0,0 +1,18 @@
+package com.cod.market.member.entity;
+
+import lombok.Getter;
+
+@Getter
+
+public enum MemberRole {
+
+    ADMIN("ADMIN"),
+    MEMBER("MEMBER");
+
+    private String value;
+    MemberRole(String value){
+        this.value = value;
+    }
+
+}
+
